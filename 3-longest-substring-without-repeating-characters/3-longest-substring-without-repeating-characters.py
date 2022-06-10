72d736acb67c529f77f2aa6d83956cc3421aa0c0
Haojun Yuan <haojundev@gmail.com>
2022-06-09 21:03:44 -0400

    Time: 131 ms (28.94%), Space: 13.9 MB (99.54%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -7,8 +7,6 @@ class Solution:
         for i in range(len(s)):
             if s[i] in dic:
                 left=max(left,dic[s[i]]+1)
-            
             res=max(res,i-left+1)
             dic[s[i]]=i
         return res
-            
