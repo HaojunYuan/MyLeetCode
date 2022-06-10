7f45ccc8b57bd79f3cbdb85ae2d87961951f6216
Haojun Yuan <haojundev@gmail.com>
2022-06-09 21:39:19 -0400

    Time: 99 ms (85.33%), Space: 14.2 MB (24.84%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,33 +1,30 @@
 class Solution:
-    def findMedianSortedArrays(self, A, B):
-        l = len(A) + len(B)
-        if l % 2 == 1:
-            return self.kth(A, B, l // 2)
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        l=len(nums1)+len(nums2)
+        if l%2==1:
+            return self.kth(nums1,nums2,l//2)
         else:
-            return (self.kth(A, B, l // 2) + self.kth(A, B, l // 2 - 1)) / 2.   
-
-    def kth(self, a, b, k):
+            return (self.kth(nums1,nums2,l//2)+self.kth(nums1,nums2,l//2-1))/2
+        
+    def kth(self,a,b,k):
         if not a:
             return b[k]
         if not b:
             return a[k]
-        ia, ib = len(a) // 2 , len(b) // 2
-        ma, mb = a[ia], b[ib]
-
-        # when k is bigger than the sum of a and b's median indices 
-        if ia + ib < k:
-            # if a's median is bigger than b's, b's first half doesn't include k
-            if ma > mb:
-                return self.kth(a, b[ib + 1:], k - ib - 1)
+        ia=len(a)//2
+        ib=len(b)//2
+        ma=a[ia]
+        mb=b[ib]
+        
+        if ia+ib<k:
+            if ma<mb:
+                return self.kth(a[ia+1:],b,k-ia-1)
             else:
-                return self.kth(a[ia + 1:], b, k - ia - 1)
-        # when k is smaller than the sum of a and b's indices
+                return self.kth(a,b[ib+1:],k-ib-1)
         else:
-            # if a's median is bigger than b's, a's second half doesn't include k
-            if ma > mb:
-                return self.kth(a[:ia], b, k)
+            if ma<mb:
+                return self.kth(a,b[:ib],k)
             else:
-                return self.kth(a, b[:ib], k)
-
+                return self.kth(a[:ia],b,k)
                 
         
