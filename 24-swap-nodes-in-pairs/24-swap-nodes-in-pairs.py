95532691df4e31a419899dce16747b6ab7f575ab
Haojun Yuan <haojundev@gmail.com>
2022-06-12 15:25:57 -0400

    Time: 54 ms (25.58%), Space: 13.9 MB (17.30%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -7,13 +7,10 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy=pre=ListNode(0,head)
         while pre.next and pre.next.next:
-            #Getting the two nodes
             a=pre.next
             b=pre.next.next
-            #Swapping
             pre.next=b
             a.next=b.next
             b.next=a
-            #Move the pre pointer to a
             pre=a
         return dummy.next
