b1cced45356977c5d452bf04a20db5ee56a5af56
Haojun Yuan <haojundev@gmail.com>
2022-06-12 14:43:37 -0400

    Time: 50 ms (50.85%), Space: 14.2 MB (75.89%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -4,5 +4,5 @@ class Solution:
         dp[0].append("")
         for i in range(n+1):
             for j in range(i):
-                dp[i]+=['('+x+')'+y for x in dp[j] for y in dp[i-1-j]]
+                dp[i]+=["("+x+")"+y for x in dp[j] for y in dp[i-1-j]]
         return dp[n]
