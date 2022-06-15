1f5135ac712d31d80b408d6454bb90365a8a8f1e
Haojun Yuan <haojundev@gmail.com>
2022-06-14 20:14:15 -0400

    Time: 184 ms (18.09%), Space: 21.8 MB (59.24%) - LeetHub

--- 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
@@ -2,16 +2,19 @@ class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
         self.dic=defaultdict(list)
+        #Each key stores all indices of the word
         for i,w in enumerate(wordsDict):
             self.dic[w].append(i)
             
 
     def shortest(self, word1: str, word2: str) -> int:
+        #Get the lists of indices of the two words
         list1,list2=self.dic[word1],self.dic[word2]
         i=j=0
         res=math.inf
         while i<len(list1) and j<len(list2):
             res=min(res,abs(list1[i]-list2[j]))
+            #Move pointers in turn
             if list1[i]<list2[j]:
                 i+=1
             else:
