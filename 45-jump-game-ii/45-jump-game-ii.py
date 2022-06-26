d7f43e7e42407a8eb61558b1da3bff7a53433485
Haojun Yuan <haojundev@gmail.com>
2022-06-26 13:50:56 -0400

    Time: 215 ms (56.29%), Space: 15 MB (89.41%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums)<=1: return 0
+        #right is the farthest point we can reach with current steps
+        left,right=0,nums[0]
+        res=1
+        while right<len(nums)-1:
+            res+=1
+            nxt=max(i+nums[i] for i in range(left,right+1))
+            left,right=right,nxt
+        return res
