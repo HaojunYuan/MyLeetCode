e7ff6609df2303447bf5a821b387429b8674b6d2
Haojun Yuan <haojundev@gmail.com>
2022-07-04 16:41:45 -0400

    Time: 54 ms (33.44%), Space: 13.8 MB (69.72%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,7 +5,7 @@ class Solution:
         for char in s:
             if char in dic:
                 stack.append(char)
-            elif stack!=[] and dic[stack[-1]]==char:
+            elif stack and dic[stack[-1]]==char:
                 stack.pop()
             else:
                 return False
