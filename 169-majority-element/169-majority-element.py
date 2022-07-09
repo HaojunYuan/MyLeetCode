7193ad2ff4ddd131aec22d4764199012db4b8e12
Haojun Yuan <haojundev@gmail.com>
2022-07-09 15:47:42 -0400

    Time: 237 ms (57.54%), Space: 15.4 MB (85.97%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,6 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic=collections.Counter(nums)
-        for num in dic:
-            if dic[num]>len(nums)//2:
-                return num
+        # dic=collections.Counter(nums)
+        # for num in dic:
+        #     if dic[num]>len(nums)//2:
+        #         return num
+        # Boyer-Moore Voting Algorithm
+        count=0
+        candidate=None
+        
+        for num in nums:
+            if count==0:
+                candidate=num
+            count+=(1 if num==candidate else -1)
+        return candidate
