9627f6e17c07c9cca327626aef58b24b226c0223
Haojun Yuan <haojundev@gmail.com>
2022-07-20 22:39:02 -0400

    Time: 117 ms (78.39%), Space: 15.6 MB (76.06%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,35 +1,29 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        graph = [[] for _ in range(numCourses)]
-        visited = [0 for _ in range(numCourses)]
-        # create graph
-        for pair in prerequisites:
-            x, y = pair
-            graph[y].append(x)
-        # visit each node
-        for i in range(numCourses):
-            if not self.dfs(graph, visited, i):
-                return False
-        return True
-    
-    def dfs(self, graph, visited, i):
-        # if ith node is marked as being visited, then a cycle is found
-        if visited[i] == -1:
-            return False
-        # if it is done visted, then do not visit again
-        if visited[i] == 1:
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
             return True
-        # mark as being visited
-        visited[i] = -1
-        # visit all the neighbours
-        for j in graph[i]:
-            if not self.dfs(graph, visited, j):
-                return False
-        # after visit all the neighbours, mark it as done visited
-        visited[i] = 1
-        return True
+        
+        indegree=[0]*numCourses
+        dic={i:[] for i in range(numCourses)}
+        
+        for child, parent in prerequisites:
+            dic[parent].append(child)
+            indegree[child]+=1
+        
+        dq=deque()
+        
+        for i in range(len(indegree)):
+            if indegree[i]==0:
+                dq.append(i)
+                
+        count=0
+        while dq:
+            course=dq.popleft()
+            count+=1
+            for child in dic[course]:
+                indegree[child]-=1
+                if indegree[child]==0:
+                    dq.append(child)
+        
+        return count==numCourses
+            
