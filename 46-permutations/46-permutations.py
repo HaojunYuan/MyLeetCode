e5aaddb895c8d744aebfc218ff8b5abbf8950b16
Haojun Yuan <haojundev@gmail.com>
2022-07-28 13:10:43 -0400

    Time: 49 ms (77.65%), Space: 14.1 MB (21.84%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,12 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-        self.dfs(nums,[],res)
-        return res
-    def dfs(self,nums,path,res):
-        if not nums:
-            res.append(path)
-            return
-        for i in range(len(nums)):
-            self.dfs(nums[:i]+nums[i+1:],path+[nums[i]],res)
+        def backtrack(first = 0):
+            # if all integers are used up
+            if first == n:  
+                output.append(nums[:])
+            for i in range(first, n):
+                # place i-th integer first 
+                # in the current permutation
+                nums[first], nums[i] = nums[i], nums[first]
+                # use next integers to complete the permutations
+                backtrack(first + 1)
+                # backtrack
+                nums[first], nums[i] = nums[i], nums[first]
+        
+        n = len(nums)
+        output = []
+        backtrack()
+        return output
         
