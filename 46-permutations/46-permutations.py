bf9293960304fb4971834c4315d5782f72b128b3
Haojun Yuan <haojundev@gmail.com>
2022-07-29 22:06:50 -0400

    Time: 41 ms (94.08%), Space: 14.1 MB (22.26%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,23 +1,16 @@
 class Solution:
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        def backtrack(first = 0):
-            # if all integers are used up
-            if first == n:  
-                output.append(nums[:])
-            for i in range(first, n):
-                # place i-th integer first 
-                # in the current permutation
-                nums[first], nums[i] = nums[i], nums[first]
-                # use next integers to complete the permutations
-                backtrack(first + 1)
-                # backtrack
-                nums[first], nums[i] = nums[i], nums[first]
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums) 
+        output=[]
         
-        n = len(nums)
-        output = []
-        backtrack()
+        def helper(first=0):
+            if first==n:
+                output.append(nums[:])
+            for i in range(first,n):
+                nums[first],nums[i]=nums[i],nums[first]
+                helper(first+1)
+                nums[first],nums[i]=nums[i],nums[first]
+                
+
+        helper()
         return output
