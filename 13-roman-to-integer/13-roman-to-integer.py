5a47841a3fecd3e996a45a0d7c2c28c0e917d92c
Haojun Yuan <haojundev@gmail.com>
2022-07-31 19:21:58 -0400

    Time: 68 ms (65.78%), Space: 13.9 MB (76.61%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40, 
+            "XC": 90,
+            "CD": 400,
+            "CM": 900
+        }
+        pre=0
+        res=values[s[0]]
+        
+        for i in range(1,len(s)):
+            if values[s[pre]]<values[s[i]]:
+                res=res-values[s[pre]]-values[s[pre]]
+                res+=values[s[i]]
+            else:
+                res+=values[s[i]]
+            pre=i
+        
+        return res
+            
