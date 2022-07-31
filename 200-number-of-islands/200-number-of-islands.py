c27a6c68fe85924337e944acd7b3175cdc0f16a7
Haojun Yuan <haojundev@gmail.com>
2022-07-31 19:36:45 -0400

    Time: 622 ms (21.58%), Space: 16.5 MB (64.70%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def helper(grid,i,j):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='1':
-                return
-            grid[i][j]='#'
-            helper(grid,i-1,j)
-            helper(grid,i+1,j)
-            helper(grid,i,j+1)
-            helper(grid,i,j-1)
-
-        if not grid:
-            return 0
-        
-        count=0
+        res=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]=='1':
-                    helper(grid,i,j)
-                    count+=1
-        return count
+                    res+=1
+                    self.helper(grid,i,j)
+        return res
+    
+    def helper(self,grid,i,j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='1':
+            return
+        grid[i][j]='#'
+        self.helper(grid,i+1,j)
+        self.helper(grid,i-1,j)
+        self.helper(grid,i,j+1)
+        self.helper(grid,i,j-1)
+        return 
+        
+        
+        
