9d0f32255500f6a68b53b3c720bcaca1bad97173
Haojun Yuan <haojundev@gmail.com>
2022-08-02 16:20:06 -0400

    Time: 96 ms (32.82%), Space: 14 MB (40.01%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        counter = collections.Counter()
+        for domain in cpdomains:
+            count, domain = domain.split()
+            count = int(count)
+            frags = domain.split('.')
+            for i in range(len(frags)):
+                counter[".".join(frags[i:])] += count
+
+        return [" ".join((str(v), k)) for k, v in counter.items()]
