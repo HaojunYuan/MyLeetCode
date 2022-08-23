399a7619f4334e24a1ed369eb1a273497800acd5
Haojun Yuan <haojundev@gmail.com>
2022-08-23 00:15:52 -0400

    Time: 59 ms (52.02%), Space: 14.3 MB (98.14%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -7,30 +7,19 @@ class Node:
 """
 
 class Solution:
-    def __init__(self):
-        self.clone={}
-        
     def cloneGraph(self, node: 'Node') -> 'Node':
+        clone={}
         if not node:
             return node
-        if node in self.clone:
-            return self.clone[node]
+        dq=deque([node])
+        clone[node]=Node(node.val,[])
         
-        self.clone[node]=Node(node.val,[])
-        self.clone[node].neighbors=[self.cloneGraph(n) for n in node.neighbors]
-        return self.clone[node]
-    
-#         clone={}
-#         if not node:
-#             return node
-#         dq=deque([node])
-#         clone[node]=Node(node.val,[])
+        while dq:
+            curr=dq.popleft()
+            for n in curr.neighbors:
+                if n not in clone:
+                    clone[n]=Node(n.val,[])
+                    dq.append(n)
+                clone[curr].neighbors.append(clone[n])
         
-#         while dq:
-#             temp=dq.popleft()
-#             for n in temp.neighbors:
-#                 if n not in clone:
-#                     clone[n]=Node(n.val,[])
-#                     dq.append(n)
-#                 clone[temp].neighbors.append(clone[n])
-#         return clone[node]
+        return clone[node]
