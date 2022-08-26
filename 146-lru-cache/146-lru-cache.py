706aeee3b2a1b36033b5fee751a0aed37161727a
Haojun Yuan <haojundev@gmail.com>
2022-08-26 18:40:17 -0400

    Time: 2225 ms (8.34%), Space: 75.8 MB (42.89%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,24 +1,11 @@
 class Node:
     def __init__(self,key=0,value=0):
         self.key=key
-        self.value=value
+        self.val=value
         self.pre=None
         self.next=None
-
+        
 class LRUCache:
-    def addToFront(self,node):
-        node.pre=self.head
-        node.next=self.head.next
-        
-        self.head.next.pre=node
-        self.head.next=node
-    
-    def remove(self,node):
-        pre=node.pre
-        nextNode=node.next
-        
-        pre.next=nextNode
-        nextNode.pre=pre
 
     def __init__(self, capacity: int):
         self.capacity=capacity
@@ -27,26 +14,35 @@ class LRUCache:
         self.head.next=self.tail
         self.tail.pre=self.head
         
+    def addToFront(self,node):
+        node.pre=self.head
+        node.next=self.head.next
+        
+        self.head.next.pre=node
+        self.head.next=node
+    
+    def remove(self,node):
+        node.pre.next=node.next
+        node.next.pre=node.pre
 
     def get(self, key: int) -> int:
         if key in self.cache:
             node=self.cache[key]
             self.remove(node)
             self.addToFront(node)
-            return node.value
+            return node.val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        n = Node(key, value)
-        self.addToFront(n)
-        self.cache[key] = n
-        if len(self.cache) > self.capacity:
-            n = self.tail.pre
+        newNode=Node(key,value)
+        self.addToFront(newNode)
+        self.cache[key]=newNode
+        if len(self.cache)>self.capacity:
+            n=self.tail.pre
             self.remove(n)
             self.cache.pop(n.key,None)
-            
 
 
 # Your LRUCache object will be instantiated and called as such:
