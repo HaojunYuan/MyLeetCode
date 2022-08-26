536feff95ad3b0cb3be291deec572e6d5e54c5f4
Haojun Yuan <haojundev@gmail.com>
2022-08-26 19:16:55 -0400

    Time: 51 ms (96.58%), Space: 14.3 MB (97.43%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,12 +1,14 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        graph=defaultdict(list)
+        pattern=defaultdict(list)
         for u, t, w in sorted(zip(username, timestamp, website)):
-            graph[u].append(w)
-        
+            pattern[u].append(w)
+            
+
+            
         counter = Counter()
-        for user, path in graph.items():
-            for triple in set(itertools.combinations(path, 3)):
+        for u, routes in pattern.items():
+            for triple in set(itertools.combinations(routes, 3)):
                 counter[triple]+=1
         
         res=None
