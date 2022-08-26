98d3c641b54c6791bb478861721adff94dcca6ee
Haojun Yuan <haojundev@gmail.com>
2022-08-26 19:19:14 -0400

    Time: 47 ms (98.78%), Space: 14.5 MB (85.00%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,10 +1,10 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         pattern=defaultdict(list)
-        for user, time, web in sorted(zip(username, timestamp, website)):
+        for user,time,web in sorted(zip(username, timestamp,website)):
             pattern[user].append(web)
-            
-        counter = Counter()
+        
+        counter=Counter()
         for user, path in pattern.items():
             for triple in set(itertools.combinations(path, 3)):
                 counter[triple]+=1
@@ -18,3 +18,4 @@ class Solution:
             elif count==maxCount and pat<res:
                 res=pat
         return res
+        
