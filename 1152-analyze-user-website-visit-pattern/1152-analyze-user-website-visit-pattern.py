04736cde6c53fa658012b053b282b09b4c6b2a74
Haojun Yuan <haojundev@gmail.com>
2022-08-26 19:24:35 -0400

    Time: 60 ms (86.90%), Space: 14.5 MB (66.18%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -1,11 +1,12 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
         pattern=defaultdict(list)
-        for user,time,web in sorted(zip(username, timestamp,website)):
+        for user, time, web in sorted(zip(username, timestamp, website)):
             pattern[user].append(web)
-        
-        counter=Counter()
+            
+        counter = Counter()
         for user, path in pattern.items():
+            # Since the score is the number of users x that visited the pattern, a user who visited one pattern multiple times only contribut to 1 score. So we need to use set to prevent duplicates.
             for triple in set(itertools.combinations(path, 3)):
                 counter[triple]+=1
         
@@ -18,4 +19,3 @@ class Solution:
             elif count==maxCount and pat<res:
                 res=pat
         return res
-        
