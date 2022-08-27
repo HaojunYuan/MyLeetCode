675b9415c4e5c32e929aa1d4ced6cbd56bba2bd2
Haojun Yuan <haojundev@gmail.com>
2022-08-27 18:20:15 -0400

    Time: 42 ms (97.55%), Space: 16.4 MB (80.61%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def helper(node,low=-math.inf,high=math.inf):
+        def helper(node,low=-math.inf, high=math.inf):
             if not node:
                 return True
             elif node.val<=low or node.val>=high:
@@ -15,4 +15,3 @@ class Solution:
                 return helper(node.left,low,node.val) and helper(node.right,node.val,high)
         
         return helper(root)
-            
