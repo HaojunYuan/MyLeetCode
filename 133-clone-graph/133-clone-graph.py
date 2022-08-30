8a1319bcb3f873caecc2e9a8b70803f806cc4fbf
Haojun Yuan <haojundev@gmail.com>
2022-08-30 19:27:15 -0400

    Time: 54 ms (63.51%), Space: 14.3 MB (77.86%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -7,20 +7,33 @@ class Node:
 """
 
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        #bfs
-        if not node:
-            return node
-        dq=deque([node])
+    def cloneGraph(self, node: 'Node') -> 'Node':\
+        #dfs
         clone={}
-        while dq:
-            temp=dq.popleft()
-            if temp not in clone:
-                clone[temp]=Node(temp.val,[])
-            for n in temp.neighbors:
-                if n not in clone:
-                    clone[n]=Node(n.val,[])
-                    dq.append(n)
-                clone[temp].neighbors.append(clone[n])
-        return clone[node]
+        def helper(node):
+            if not node:
+                return node
+            elif node in clone:
+                return clone[node]
+            clone[node]=Node(node.val,[])
+            clone[node].neighbors=[helper(n) for n in node.neighbors]
+            return clone[node]
+        return helper(node)
+
+        
+        # #bfs
+        # if not node:
+        #     return node
+        # dq=deque([node])
+        # clone={}
+        # while dq:
+        #     temp=dq.popleft()
+        #     if temp not in clone:
+        #         clone[temp]=Node(temp.val,[])
+        #     for n in temp.neighbors:
+        #         if n not in clone:
+        #             clone[n]=Node(n.val,[])
+        #             dq.append(n)
+        #         clone[temp].neighbors.append(clone[n])
+        # return clone[node]
         
