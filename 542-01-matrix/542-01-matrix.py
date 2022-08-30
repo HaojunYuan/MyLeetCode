37cca2074fad0f4d46eab9934e82ed9c25d59fee
Haojun Yuan <haojundev@gmail.com>
2022-08-30 17:16:17 -0400

    Time: 1375 ms (19.78%), Space: 16.5 MB (97.85%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,19 +1,17 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-
-        for r in range(m):
-            for c in range(n):
-                if mat[r][c] > 0:
-                    top = mat[r - 1][c] if r > 0 else math.inf
-                    left = mat[r][c - 1] if c > 0 else math.inf
-                    mat[r][c] = min(top, left) + 1
-
-        for r in range(m - 1, -1, -1):
-            for c in range(n - 1, -1, -1):
-                if mat[r][c] > 0:
-                    bottom = mat[r + 1][c] if r < m - 1 else math.inf
-                    right = mat[r][c + 1] if c < n - 1 else math.inf
-                    mat[r][c] = min(mat[r][c], bottom + 1, right + 1)
-
+        m,n=len(mat),len(mat[0])
+        
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]!=0:
+                    top=mat[i-1][j] if i>0 else math.inf
+                    left=mat[i][j-1] if j>0 else math.inf
+                    mat[i][j]=1+min(top,left)
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if mat[i][j]!=0:
+                    bottom=mat[i+1][j] if i<m-1 else math.inf
+                    right=mat[i][j+1] if j<n-1 else math.inf
+                    mat[i][j]=min(mat[i][j],bottom+1,right+1)
         return mat
