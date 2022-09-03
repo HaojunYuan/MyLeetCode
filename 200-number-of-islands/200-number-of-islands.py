46545e0d2720317adac0049bb7fe0acdbb1cb5c2
Haojun Yuan <haojundev@gmail.com>
2022-09-03 19:27:14 -0400

    Time: 304 ms (94.74%), Space: 16.4 MB (81.30%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,15 +4,14 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]=='1':
-                    self.helper(grid,i,j)
+                    self.bfs(grid,i,j)
                     res+=1
         return res
-    
-    def helper(self,grid,i,j):
+    def bfs(self,grid,i,j):
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='1':
             return
         grid[i][j]='0'
-        self.helper(grid,i+1,j)
-        self.helper(grid,i-1,j)   
-        self.helper(grid,i,j+1)
-        self.helper(grid,i,j-1)                
+        self.bfs(grid,i+1,j)
+        self.bfs(grid,i-1,j)    
+        self.bfs(grid,i,j+1) 
+        self.bfs(grid,i,j-1)                
