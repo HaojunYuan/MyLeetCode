d833bacc1c383cb0eaf4290637d524613ed8f0d5
Haojun Yuan <haojundev@gmail.com>
2022-09-06 15:25:05 -0400

    Time: 143 ms (66.00%), Space: 15.6 MB (61.73%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,25 +1,24 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        dic=defaultdict(list)
         indegree=[0]*numCourses
-        dic={i:[] for i in range(numCourses)}
-        res=[]
-        
-        for child,parent in prerequisites:
-            indegree[child]+=1
+        for child, parent in prerequisites:
             dic[parent].append(child)
+            indegree[child]+=1
         
         dq=deque()
         for i in range(len(indegree)):
             if indegree[i]==0:
                 dq.append(i)
         
+        res=[]
         while dq:
-            parent=dq.popleft()
-            res.append(parent)
-            for child in dic[parent]:
+            course=dq.popleft()
+            res.append(course)
+            for child in dic[course]:
                 indegree[child]-=1
                 if indegree[child]==0:
                     dq.append(child)
-                    
+        
         return res if len(res)==numCourses else []
         
