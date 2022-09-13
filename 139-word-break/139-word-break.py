fb518fcddbd799c2295dceb9005336932465de7f
Haojun Yuan <haojundev@gmail.com>
2022-09-13 18:29:46 -0400

    Time: 92 ms (9.79%), Space: 14.1 MB (43.82%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,9 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        d = [False] * len(s)    
-        for i in range(len(s)):
-            for w in wordDict:
-                #d[i-len(w)] checks if there is a word ends before the start of                     curent word, and i-len(w) == -1 checks if the word starts from the                   beginnig of the string
-                if w == s[i-len(w)+1:i+1] and (d[i-len(w)] or i-len(w) == -1):
-                    d[i] = True
-        return d[-1]
+        words=set(wordDict)
+        dp=[False]*(len(s)+1)
+        dp[0]=True
+        
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i]=True
+                    break
+        return dp[len(s)]
