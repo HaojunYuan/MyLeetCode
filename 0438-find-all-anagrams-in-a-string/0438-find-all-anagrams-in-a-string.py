52f1d2dc7c85fa164dd7b286fc9e511f061b9708
Haojun Yuan <haojundev@gmail.com>
2022-11-15 17:33:06 -0500

    Time: 342 ms (46.59%), Space: 15.2 MB (81.44%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,21 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        c=Counter(p)
+        counter=Counter(p)
         l,r=0,0
-        count=len(p)
         res=[]
+        count=len(p)
         while r<len(s):
-            if c[s[r]]>=1:
+            if counter[s[r]]>0:
                 count-=1
-            c[s[r]]-=1
+            counter[s[r]]-=1
             r+=1
             if count==0:
                 res.append(l)
-            if r-l==len(p): #No matter if we found an index or not, we need to move the sliding window as soon as we reach the maximum size.
-                if c[s[l]]>=0: #If s[l] is not in p, it would go negative. This indicates that s[l] is in p and therefore we need to increase count by 1, since after moving the left pointer 1 index to the right, we have 1 more character to match
+            if r-l==len(p):
+                if counter[s[l]]>=0:
                     count+=1
-                c[s[l]]+=1
+                counter[s[l]]+=1
                 l+=1
         return res
-                
-            
