b099ca3280fe15f0bafbe6096965fd545712e6b1
Haojun Yuan <haojundev@gmail.com>
2022-12-01 16:57:15 -0500

    Time: 403 ms (36.83%), Space: 15.2 MB (81.54%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -3,17 +3,17 @@ class Solution:
         counter=Counter(p)
         l,r=0,0
         res=[]
-        target=len(p)
+        missing=len(p)
         while r<len(s):
             if counter[s[r]]>0:
-                target-=1
+                missing-=1
             counter[s[r]]-=1
             r+=1
-            if target==0:
+            if missing==0:
                 res.append(l)
             if r-l==len(p):
                 if counter[s[l]]>=0:
-                    target+=1
+                    missing+=1
                 counter[s[l]]+=1
                 l+=1
         return res
