f985b4f14e5d642e5fd7dc65fdfe48d9cd598345
Haojun Yuan <haojundev@gmail.com>
2022-12-01 17:02:42 -0500

    Time: 443 ms (30.54%), Space: 14.8 MB (37.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,18 +2,18 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         counter=Counter(t)
         l,r=0,0
-        start,end=0,0
         missing=len(t)
-        minLen=math.inf
+        minlen=math.inf
+        start,end=0,0
         while r<len(s):
             if counter[s[r]]>0:
                 missing-=1
             counter[s[r]]-=1
             r+=1
             while missing==0:
-                if r-l<minLen:
+                if r-l<minlen:
+                    minlen=r-l
                     start,end=l,r
-                    minLen=r-l
                 if counter[s[l]]>=0:
                     missing+=1
                 counter[s[l]]+=1
