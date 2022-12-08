c345d37036cec2ac02b543b612955cf1717675c9
Haojun Yuan <haojundev@gmail.com>
2022-12-07 20:35:06 -0500

    Time: 934 ms (96.77%), Space: 36.3 MB (50.78%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,9 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        time = [float(target - p) / s for p, s in sorted(zip(position, speed))]
-        res = cur = 0
+        time=[]
+        for p,s in sorted(zip(position,speed)):
+            time.append(float((target-p)/s))
+        res=curr=0
         for t in time[::-1]:
-            if t > cur:
-                res += 1
-                cur = t
+            if t>curr:
+                res+=1
+                curr=t
         return res
