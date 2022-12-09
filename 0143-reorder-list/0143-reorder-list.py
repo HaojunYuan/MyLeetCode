2ba4989b51a15414b210a99daf1dbea16f4b62ab
Haojun Yuan <haojundev@gmail.com>
2022-12-09 17:05:59 -0500

    Time: 232 ms (25.12%), Space: 23.9 MB (65.40%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -4,27 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reorderList(self, head: ListNode) -> None:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
         if not head:
-            return 
-        
-        # find the middle of linked list [Problem 876]
-        # in 1->2->3->4->5->6 find 4 
-        slow = fast = head
+            return
+        slow=fast=head
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next 
-            
-        # reverse the second part of the list [Problem 206]
-        # convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
-        # reverse the second half in-place
-        prev, curr = None, slow
+            slow=slow.next
+            fast=fast.next.next
+        
+        pre=None
+        curr=slow
         while curr:
-            curr.next, prev, curr = prev, curr, curr.next       
-
-        # merge two sorted linked lists [Problem 21]
-        # merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
-        first, second = head, prev
-        while second.next:
-            first.next, first = second, first.next
-            second.next, second = first, second.next
+            temp=curr.next
+            curr.next=pre
+            pre=curr
+            curr=temp
+        
+        l1,l2=head,pre
+        while l2.next:
+            l1.next,l1=l2,l1.next
+            l2.next,l2=l1,l2.next
