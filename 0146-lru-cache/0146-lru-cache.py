23f122ff86402e2aea7bade2486397e395538043
Haojun Yuan <haojundev@gmail.com>
2022-12-10 18:27:15 -0500

    Time: 2333 ms (19.22%), Space: 75.7 MB (47.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,47 +1,47 @@
 class Node:
     def __init__(self,key=0,value=0):
-        self.key=key 
-        self.value=value 
+        self.key=key
+        self.val=value
         self.pre=None
         self.next=None
-
+        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity=capacity
         self.cache={}
+        self.capacity=capacity
         self.head,self.tail=Node(),Node()
-        self.head.next=self.tail 
+        self.head.next=self.tail
         self.tail.pre=self.head
     
-    def addToFront(self,node):
-        node.next=self.head.next 
-        node.pre=self.head 
-        self.head.next.pre=node 
-        self.head.next=node 
+    def insert(self,node):
+        node.pre=self.head
+        node.next=self.head.next
+        self.head.next.pre=node
+        self.head.next=node
+    
     def remove(self,node):
-        node.next.pre=node.pre 
+        node.next.pre=node.pre
         node.pre.next=node.next
-        
+
     def get(self, key: int) -> int:
         if key in self.cache:
             node=self.cache[key]
             self.remove(node)
-            self.addToFront(node)
-            return node.value
+            self.insert(node)
+            return node.val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         newNode=Node(key,value)
-        self.addToFront(newNode)
         self.cache[key]=newNode
+        self.insert(newNode)
         if len(self.cache)>self.capacity:
-            gone=self.tail.pre
-            self.remove(gone)
-            self.cache.pop(gone.key,None)
-
+            toRemove=self.tail.pre
+            self.remove(toRemove)
+            self.cache.pop(toRemove.key,None)
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
