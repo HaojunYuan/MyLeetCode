11c45daa216d03508e9004243d058ec794d35397
Haojun Yuan <haojundev@gmail.com>
2023-05-21 12:28:14 -0400

    Time: 51 ms (12.41%), Space: 16.5 MB (16.53%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -10,6 +10,4 @@ class Solution:
             return True
         elif not p or not q:
             return False
-        elif p.val!=q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right)
+        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
