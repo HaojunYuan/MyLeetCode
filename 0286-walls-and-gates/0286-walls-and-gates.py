a3707ce46eef07e33e53229eddd0101bdd519d53
Haojun Yuan <haojundev@gmail.com>
2023-05-21 19:41:47 -0400

    Time: 320 ms (26.86%), Space: 24 MB (15.59%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,26 +3,21 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        # Start from each gate, do DFS
-        # Each gate is marked as 0. Also, the distance between the gate and itself is 0. 
-        # We pass in a variable d=0 when we enter dfs.
-        # For surrounding rooms, we can pass d+1 to the room.
-        rows, cols=len(rooms),len(rooms[0])
-        
-        def dfs(r,c,d):
+        # starting from each gate, mark the rooms with its distance to nearest gates
+        # we only update the distance if the new distance is smaller than the current distance of the room (to avoid duplication and infinite loop)
+        rows,cols=len(rooms),len(rooms[0])
+        def bfs(r,c,d):
             if r<0 or c<0 or r==rows or c==cols or rooms[r][c]==-1:
                 return
             if d>0 and rooms[r][c]<=d:
                 return
             rooms[r][c]=d
-            dfs(r-1,c,d+1)
-            dfs(r+1,c,d+1)
-            dfs(r,c+1,d+1)
-            dfs(r,c-1,d+1)
-            
-        
+            bfs(r+1,c,d+1)
+            bfs(r-1,c,d+1)
+            bfs(r,c+1,d+1)
+            bfs(r,c-1,d+1)
         for r in range(rows):
             for c in range(cols):
                 if rooms[r][c]==0:
-                    dfs(r,c,0)
-        return rooms
+                    bfs(r,c,0)
+                             
