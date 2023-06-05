64751031f4eb4fd9f80a79972d7f8b026c6e4951
Haojun Yuan <haojundev@gmail.com>
2023-06-05 01:06:52 -0400

    Time: 1246 ms (74.19%), Space: 44.9 MB (68.92%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,15 +1,19 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        subs=defaultdict(list)
+        # the time is the maximum time needed to reach from root to a leaf node
+        # time=time to reach the current node + informTime
+        # build an manager-subordinate hashmap
         self.res=0
+        dic=defaultdict(list)
         for i,m in enumerate(manager):
-            subs[m].append(i)
+            dic[m].append(i)
         
         # bfs
         q=deque([(headID,0)])
         while q:
             m,time=q.popleft()
             self.res=max(self.res,time)
-            for s in subs[m]:
-                q.append((s,time + informTime[m]))
+            for s in dic[m]:
+                q.append((s,time+informTime[m]))
         return self.res
+            
