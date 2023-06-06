d9aeb40494e6e2d18d80f6f55fe86d3b3f944a01
Haojun Yuan <haojundev@gmail.com>
2023-06-06 02:41:26 -0400

    Time: 52 ms (7.64%), Space: 16.3 MB (16.75%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,14 +1,11 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        # need to invert stones since we want to pop the largest weights
         for i in range(len(stones)):
             stones[i]*=-1
         heapq.heapify(stones)
-        # at most 1 stone will left
         while len(stones)>1:
-            first=heapq.heappop(stones)
-            second=heapq.heappop(stones)
-            if second!=first:
-                heapq.heappush(stones,first-second)
+            stone1=heapq.heappop(stones)
+            stone2=heapq.heappop(stones)
+            if stone1!=stone2:
+                heapq.heappush(stones,stone1-stone2)
         return -stones[0] if stones else 0
-        
