742f528526b7a6da419e00d84c03a377954e775d
Haojun Yuan <haojundev@gmail.com>
2023-06-11 23:56:55 -0400

    Time: 120 ms (39.22%), Space: 17.7 MB (73.07%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,31 +1,31 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         # union find
-        # Each node is the parent of itself
+        # a list to store the parent for each node. A node is its own parent
         parent=[i for i in range(n)]
-        # Each node has an initial degree of 1
+        # degree of each node
         degree=[1]*n
-        # Number of components
         components=n
         
-        # Find the parent of a node
-        def find(p):
-            while p!=parent[p]:
-                p=parent[p]
-            return p
+        # find the starting parent of the node
+        def find(node):
+            while node!=parent[node]:
+                node=parent[node]
+            return node
         
+        # union two nodes based on their parent's degree
         def union(n1,n2):
             p1,p2=find(n1),find(n2)
-            if p1==p2: # Same parent already
+            # if they alr have the same parent, return False
+            if p1==p2:
                 return 0
+            if degree[p1]<degree[p2]:
+                # make p2 p1's parent
+                parent[p1]=p2
+                degree[p2]+=degree[p1]
             else:
-                if degree[p1]>degree[p2]:
-                    # Make p1 the parent of p2
-                    parent[p2]=p1
-                    degree[p1]+=degree[p2]
-                else:
-                    parent[p1]=p2
-                    degree[p2]+=degree[p1]
+                parent[p2]=p1
+                degree[p1]+=degree[p2]
             return 1
         
         for n1,n2 in edges:
