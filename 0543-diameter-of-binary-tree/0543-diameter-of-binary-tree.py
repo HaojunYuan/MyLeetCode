48a62fdc201d07655ba97f165edb34560e4ec35b
Haojun Yuan <haojundev@gmail.com>
2023-06-13 20:55:00 -0400

    Time: 43 ms (95.89%), Space: 18.7 MB (71.02%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,6 +6,9 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # depth of the left subtree + depth of the right subtree
+        # if diameter>res, update
+        # to ensure what we return to the root lead to maximum diameter, return the maximum depth of left or right subtree of each node
         self.res=0
         def helper(node):
             if not node:
