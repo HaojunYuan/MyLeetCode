5654f5ae1196f6d40223b1cc71fc0e33ed4a6954
Haojun Yuan <haojundev@gmail.com>
2023-06-24 01:03:17 -0400

    Time: 57 ms (35.88%), Space: 16.5 MB (87.60%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,24 +1,23 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # for each nums[i], we have two choices
-        # to include all duplicates of nums[i]
-        # do not include any nums[i]
+        # for each num in nums
+        # 1. include all duplicates of num
+        # 2. do not include any duplicate of num
+        
         res=[]
+        # sort the nums array so that duplicate numbers are next to each other
         nums.sort()
-        # sort the list to make sure duplicates are together
-        def dfs(i,curr):
-            # if we reach the end of the list, append the current path
+        def dfs(curr, i):
             if i==len(nums):
                 res.append(curr[:])
                 return
-            # all subsets that contain all duplicates of nums[i]
+            # include all duplicates of nums[i]
             curr.append(nums[i])
-            dfs(i+1,curr)
+            dfs(curr,i+1)
             curr.pop()
-            # all subsets that do not include any duplicates of nums[i]
+            # do not include any duplicate of nums[i]
             while i+1<len(nums) and nums[i]==nums[i+1]:
                 i+=1
-            # if all upcoming numbers are duplicate, i will reach the end of nums and just append the current path without any duplicate
-            dfs(i+1,curr)
-        dfs(0,[])
+            dfs(curr,i+1)
+        dfs([],0)
         return res
