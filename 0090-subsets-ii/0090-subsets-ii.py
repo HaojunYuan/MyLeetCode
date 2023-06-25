66a9749bd8769f077bc44e12476ab2c092ba0dd0
Haojun Yuan <haojundev@gmail.com>
2023-06-25 19:51:38 -0400

    Time: 56 ms (42.41%), Space: 16.5 MB (90.22%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,13 +1,11 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # for each num in nums
-        # 1. include all duplicates of num
-        # 2. do not include any duplicate of num
-        
+        # for each nums[i]
+        # include all duplicates of nums[i]
+        # do not include any duplicate of nums[i]
         res=[]
-        # sort the nums array so that duplicate numbers are next to each other
         nums.sort()
-        def dfs(curr, i):
+        def dfs(curr,i):
             if i==len(nums):
                 res.append(curr[:])
                 return
@@ -15,9 +13,11 @@ class Solution:
             curr.append(nums[i])
             dfs(curr,i+1)
             curr.pop()
-            # do not include any duplicate of nums[i]
+            
+            # do not include any nums[i]
             while i+1<len(nums) and nums[i]==nums[i+1]:
                 i+=1
             dfs(curr,i+1)
+        
         dfs([],0)
         return res
