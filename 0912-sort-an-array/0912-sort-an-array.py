49d5f0d5daa6df27507e6021785aed6e61cea4d6
Haojun Yuan <haojundev@gmail.com>
2023-08-05 17:04:25 -0400

    Time: 1527 ms (73.91%), Space: 25.1 MB (43.14%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,36 +1,34 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        l,r = 0, len(nums)-1
-        self.process(nums, l, r)
-        return nums
+        l, r=0, len(nums)-1
+        res=self.process(l,r,nums)
+        return res
     
-    def process(self, arr, l, r):
+    def process(self, l, r, arr):
         if l>=r:
-            return
-        mid = l+(r-l)//2
-        self.process(arr,l,mid)
-        self.process(arr, mid+1, r)
-        self.merge(arr, l, mid, r)
+            return [arr[l]]
+        mid=l+(r-l)//2
+        left=self.process(l,mid,arr)
+        right=self.process(mid+1, r, arr)
+        res=self.merge(left, right)
+        return res
     
-    def merge(self, arr,l, mid, r):
+    def merge(self, left, right):
         temp=[]
-        i, j = l, mid+1
-        
-        while i<=mid and j<=r:
-            if arr[i]<=arr[j]:
-                temp.append(arr[i])
+        i=0
+        j=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                temp.append(left[i])
                 i+=1
             else:
-                temp.append(arr[j])
+                temp.append(right[j])
                 j+=1
         
-        while i<=mid:
-            temp.append(arr[i])
+        while i<len(left):
+            temp.append(left[i])
             i+=1
-        while j<=r:
-            temp.append(arr[j])
+        while j<len(right):
+            temp.append(right[j])
             j+=1
-        
-        # replace arr with temp
-        for i in range(len(temp)):
-            arr[l+i]=temp[i]
+        return temp
