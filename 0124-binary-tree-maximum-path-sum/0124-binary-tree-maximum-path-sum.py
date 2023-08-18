44a16a5ffb98751db416e9b2371b13fab754bf36
Haojun Yuan <haojundev@gmail.com>
2023-08-18 16:22:41 -0400

    Time: 103 ms (36.20%), Space: 24.3 MB (7.90%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -4,18 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# The path may or may not pass through the root
+class Info:
+    def __init__(self, partial, total):
+        self.partial=partial
+        self.total=total
+        
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.res=-math.inf # since there are negative numbers
-        def helper(node):
-            # get max sum from left and right subtree
-            # update self.res
-            # return max(left, right)
-            if not node:
-                return 0
-            left=helper(node.left)
-            right=helper(node.right)
-            self.res=max(self.res,node.val+left+right)
-            return max(node.val+max(left,right),0)
-        helper(root)
-        return self.res
+        return self.process(root).total
+    def process(self, node):
+        if not node:
+            return Info(0,-math.inf)
+        left=self.process(node.left)
+        right=self.process(node.right)
+        left.partial=max(left.partial, 0)
+        right.partial=max(right.partial, 0)
+        
+        partial=node.val+max(left.partial, right.partial, 0)
+        total=max(left.total, right.total, left.partial+right.partial+node.val)
+        return Info(partial, total)
