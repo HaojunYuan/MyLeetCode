003c1aa4a8e35a59a9e597aeceea104e07430310
Haojun Yuan <haojundev@gmail.com>
2023-08-28 16:51:19 -0400

    Time: 2929 ms (45.46%), Space: 36 MB (67.39%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,41 +1,41 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        self.counts = [0] * len(nums)
-        self.indices = list(range(len(nums))) # to keep track of indices
-        self.mergeSort(nums, 0, len(nums) - 1)
-        return self.counts
+        # since the index of the nums will change when we merge, use a list to keep track of indices when we merge sort the nums
+        self.count=[0]*len(nums)
+        self.indices = [i for i in range(len(nums))]
+        l,r = 0,len(nums)-1
+        self.process(l,r,nums)
+        return self.count
     
-    def mergeSort(self, nums, l, r):
-        if l >= r:
+    def process(self, l, r, nums):
+        if l>=r:
             return
-        mid = l + (r - l) // 2
-        self.mergeSort(nums, l, mid)
-        self.mergeSort(nums, mid + 1, r)
-        self.merge(nums, l, mid, r)
+        mid = l+(r-l)//2
+        self.process(l, mid, nums)
+        self.process(mid+1, r, nums)
+        self.merge(l,mid,r,nums)
     
-    def merge(self, nums, l, mid, r):
-        temp = [0] * (r - l + 1)
-        i, j, k = l, mid + 1, 0
-
-        while i <= mid and j <= r:
-            if nums[self.indices[i]] <= nums[self.indices[j]]:
-                temp[k] = self.indices[i]
-                self.counts[self.indices[i]] += j - (mid + 1)
-                i += 1
+    # We need to get info about smaller numbers after self when we merge
+    def merge(self, l, mid, r, nums):
+        # We sort the indices
+        temp=[]
+        i,j = l, mid+1
+        while i<=mid and j<=r:
+            if nums[self.indices[i]]<=nums[self.indices[j]]:
+            # We need to update count because we know for nums[i] how many numbers on the right is smaller than nums[i]
+                self.count[self.indices[i]]+=j-(mid+1)
+                temp.append(self.indices[i])
+                i+=1
+            # nums[i]>nums[j]:
             else:
-                temp[k] = self.indices[j]
-                j += 1
-            k += 1
-
-        while i <= mid:
-            temp[k] = self.indices[i]
-            self.counts[self.indices[i]] += r - mid
-            i += 1
-            k += 1
-        while j <= r:
-            temp[k] = self.indices[j]
-            k += 1
-            j += 1
-        
-        for i in range(l, r + 1):
-            self.indices[i] = temp[i - l]
+                temp.append(self.indices[j])
+                j+=1
+        while i<=mid:
+            self.count[self.indices[i]]+=j-mid-1
+            temp.append(self.indices[i])
+            i+=1
+        while j<=r:
+            temp.append(self.indices[j])
+            j+=1
+        for i in range(len(temp)):
+            self.indices[l+i]=temp[i]
