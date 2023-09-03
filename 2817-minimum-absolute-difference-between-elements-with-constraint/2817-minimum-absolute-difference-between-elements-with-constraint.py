278b59421cdd12db9e4531bde50aabf472692dd6
Haojun Yuan <haojundev@gmail.com>
2023-09-03 18:32:58 -0400

    Time: 1214 ms (82.58%), Space: 30.9 MB (97.10%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
@@ -1,23 +1,16 @@
-from sortedcontainers import SortedSet
-
 class Solution:
-
-    
-
-    def minAbsoluteDifference(self, nums, x):
-        ans = float('inf')
-        s = SortedSet()
-
-        for i in range(x, len(nums)):
-            s.add(nums[i - x])
-
-            it = s.bisect_left(nums[i])
-
-            if it != len(s):
-                ans = min(ans, abs(nums[i] - s[it]))
-
-            if it != 0:
-                ans = min(ans, abs(nums[i] - s[it - 1]))
-
-        return ans
-
+    def minAbsoluteDifference(self, nums: List[int], x: int) -> int:
+        tmp = sorted(nums[x:])
+        res = inf
+        for i, n in enumerate(nums):
+            if not tmp:
+                break
+            if n <= tmp[0]:
+                res = min(tmp[0] - n, res)
+            elif n >= tmp[-1]:
+                res = min(n - tmp[-1], res)
+            else:
+                idx = bisect_left(tmp, n)
+                res = min(min(abs(n - tmp[idx]), abs(n - tmp[idx-1])), res)
+            tmp.pop(bisect_left(tmp, nums[i+x]))
+        return res
