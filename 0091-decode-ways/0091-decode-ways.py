eccd23d6eac63ca44dedb3cec4fe6d525c8d6898
Haojun Yuan <haojundev@gmail.com>
2023-09-08 14:10:16 -0400

    Time: 36 ms (84.28%), Space: 16.2 MB (81.46%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,36 +1,27 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # try from left to right
+        # DP
         if not s:
             return 0
-        dp=[0]*(len(s)+1)
-        dp[len(s)]=1
+        dp = [0]*(len(s) + 1)
+        dp[len(s)] = 1
         for i in range(len(s)-1, -1, -1):
-            if s[i]=='0':
-                dp[i]=0
-                continue
-            temp=0
-            temp+=dp[i+1]
-            if i+1<len(s) and int(s[i:i+2])<27:
-                temp+=dp[i+2]
-            dp[i]=temp
+            if s[i] != '0':
+                dp[i] += dp[i+1]
+                if i+1<len(s) and int(s[i:i+2]) < 27:
+                    dp[i] += dp[i+2]
         return dp[0]
-        
         
 #         return self.process(s, 0)
     
+#     # Return the number of decode ways starting from index i
 #     def process(self, s, i):
-#         # Base case
 #         if i==len(s):
 #             return 1
 #         if s[i]=='0':
 #             return 0
-#         res=0
-#         # s[i] is a single digit
-#         res+=self.process(s, i+1)
-#         # if s and s+1 is smaller than 26, add the possibility
+#         res = 0
+#         res += self.process(s, i+1)
 #         if i+1<len(s) and int(s[i:i+2])<27:
-#             res+=self.process(s, i+2)
+#             res += self.process(s, i+2)
 #         return res
-    
-    
