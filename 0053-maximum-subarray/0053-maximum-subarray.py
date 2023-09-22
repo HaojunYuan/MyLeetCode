048efc9ef14291294cff2cae4ac641e41fd547e0
Haojun Yuan <haojundev@gmail.com>
2023-09-22 12:04:29 -0400

    Time: 593 ms (67.26%), Space: 30.6 MB (39.08%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # if the temporary sum is negative, we discard it, since adding negative number to any existing number will only make it smaller not larger
-        res=-math.inf
-        temp=0
+        res = -math.inf
+        temp = 0
         for n in nums:
-            temp+=n
-            res=max(res,temp)
-            temp=max(temp,0)
+            temp += n
+            res = max(res, temp)
+            if temp < 0:
+                temp = 0
         return res
