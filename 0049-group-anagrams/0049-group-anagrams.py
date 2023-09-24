50dc512d40cb3756babd8ab4e6088361c21d77ff
Haojun Yuan <haojundev@gmail.com>
2023-09-24 16:08:25 -0400

    Time: 98 ms (65.49%), Space: 20.6 MB (42.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # # O(n^2logn)
-        # dic={}
-        # for s in strs:
-        #     if tuple(sorted(s)) not in dic:
-        #         dic[tuple(sorted(s))]=[]
-        #     dic[tuple(sorted(s))].append(s)
-        # return dic.values()
-        
-        # O(n^2)
-        dic=collections.defaultdict(list)
-        
+        c = defaultdict(list)
         for s in strs:
-            count=[0]*26
-            for c in s:
-                count[ord(c)-ord('a')]+=1
-            dic[tuple(count)].append(s)
-        return dic.values()
+            c[tuple(sorted(list(s)))].append(s)
+        return c.values()
