63b8d5dcd7794a91f9d0025ef196de8c4c5ec69a
Haojun Yuan <haojundev@gmail.com>
2023-10-03 18:05:26 -0400

    Time: 82 ms (69.97%), Space: 17.8 MB (16.77%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # binary search
-        # 1st search: find left boundary
-        # 2nd search: find right boundary
-        left = self.searchLeft(nums, target)
-        right = self.searchRight(nums, target)
-        if 0 <= left < len(nums) and 0 <= right < len(nums) and left <= right and nums[left] == target:
-            return [left, right]
-        else:
-            return [-1, -1]
+        index=self.binary(nums,target)
+        return self.expand(nums,index)
     
-    def searchLeft(self, nums, target):
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] >= target:
-                r = mid - 1
-            else:
-                l = mid + 1
-        return l
     
-    def searchRight(self, nums, target):
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] > target:
-                r = mid - 1
+    def binary(self,nums,target):
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=l+(r-l)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                l=mid+1
             else:
-                l = mid + 1
-        return r
-                
+                r=mid-1
+        return -1
+    
+    def expand(self,nums,index):
+        if index==-1:
+            return [-1,-1]
+        l=r=index
+        while l>0 and nums[l-1]==nums[l]:
+            l-=1
+        while r<len(nums)-1 and nums[r+1]==nums[r]:
+            r+=1
+        return [l,r]
