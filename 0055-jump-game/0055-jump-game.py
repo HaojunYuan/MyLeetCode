44dc959ff9affceb5520faee35330cdc2c7fb137
Haojun Yuan <haojundev@gmail.com>
2023-10-06 20:39:24 -0400

    Time: 440 ms (27.81%), Space: 17.5 MB (79.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,23 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        max_reach = 0  # Initialize the maximum reachable position.
-
-        for i in range(n):
-            # If the current position is unreachable, return False.
-            if i > max_reach:
+        maxReach = 0
+        for i in range(len(nums)):
+            if i > maxReach:
                 return False
-            
-            # Update the maximum reachable position.
-            max_reach = max(max_reach, i + nums[i])
-
-            # If we can reach the end, return True.
-            if max_reach >= n - 1:
+            maxReach = max(maxReach, i + nums[i])
+            if maxReach >= len(nums) - 1:
                 return True
-        
-        return True  # If we reach here, we can reach the end.
-
-# Usage:
-# sol = Solution()
-# result = sol.canJump([2, 3, 1, 1, 4])
-# print(result)  # Output: True
