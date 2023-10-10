d23576b57fe5926db703ef75b68d0d3d2606db25
Haojun Yuan <haojundev@gmail.com>
2023-10-10 11:22:02 -0400

    Time: 556 ms (44.08%), Space: 30.8 MB (97.61%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,29 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        # iterate through s with a start index
-        # for j in range i, check if s[j:i+1] is palindrome
-        # append to curr
-        # dfs(i+1, curr)
-        # curr.pop()
-        
-        res=[]
-        def dfs(start, curr):
-            if start==len(s):
-                res.append(curr[:])
-                return
-            for i in range(start, len(s)):
-                if self.isPalindrome(s,start, i):
-                    curr.append(s[start:i+1])
-                    dfs(i+1, curr)
-                    curr.pop()
-        dfs(0,[])
+        res = []
+        self.process(s, 0, [], res)
         return res
+    
+    def process(self, s, start, curr, res):
+        if start == len(s):
+            res.append(curr[:])
+            return
+        for i in range(start, len(s)):
+            if self.isPalindrome(s, start, i):
+                curr.append(s[start:i + 1])
+                self.process(s, i + 1, curr, res)
+                curr.pop()
         
-        
-    def isPalindrome(self,s, l, r):
-        while l<=r:
-            if s[l]!=s[r]:
+    def isPalindrome(self, s, l, r):
+        while l <= r:
+            if s[l] != s[r]:
                 return False
-            l+=1
-            r-=1
+            l += 1
+            r -= 1
         return True
