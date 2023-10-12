d0004666d1232a43a84d656e15069083467a7bd7
Haojun Yuan <haojundev@gmail.com>
2023-10-12 00:42:32 -0400

    Time: 122 ms (78.23%), Space: 31.4 MB (78.40%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,28 +6,29 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        lenA=self.getLen(headA)
-        lenB=self.getLen(headB)
-        curr1=headA
-        curr2=headB
-        while lenA>lenB:
-            curr1=curr1.next
-            lenA-=1
-        while lenB>lenA:
-            curr2=curr2.next
-            lenB-=1
+        l1 = 0
+        l2 = 0
+        curr1 = headA
+        while curr1:
+            l1 += 1
+            curr1 = curr1.next
+            
+        curr2 = headB
+        while curr2:
+            l2 += 1
+            curr2 = curr2.next
+        
+        curr1, curr2 = headA, headB
+        if l1 < l2:
+            l1, l2 = l2, l1
+            curr1, curr2 = headB, headA
+        
+        for _ in range(l1 - l2):
+            curr1 = curr1.next
         
         while curr1 and curr2:
-            if curr1==curr2:
+            if curr1 == curr2:
                 return curr1
-            curr1=curr1.next
-            curr2=curr2.next
+            curr1 = curr1.next
+            curr2 = curr2.next
         return None
-            
-    
-    def getLen(self, head):
-        n=0
-        while head:
-            n+=1
-            head=head.next
-        return n
