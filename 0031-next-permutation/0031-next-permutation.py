4de8b8d0570fa47c94ad9c23f960935194780987
Haojun Yuan <haojundev@gmail.com>
2024-01-23 19:46:59 -0500

    Time: 45 ms (64.20%), Space: 16.6 MB (59.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -5,7 +5,7 @@ class Solution:
         """
         for i in range(len(nums) - 1, -1, -1):
             if i > 0 and nums[i] > nums[i - 1]:
-                for j in range(len(nums) - 1, i - 1, -1):
+                for j in range(len(nums) - 1, - 1, -1):
                     if nums[j] > nums[i - 1]:
                         nums[j], nums[i - 1] = nums[i - 1], nums[j]
                         nums[i:] = sorted(nums[i:])
