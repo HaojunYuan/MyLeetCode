8cdcf45ca88f7f14b0db7e11ec420d9ec8e13f97
Haojun Yuan <haojundev@gmail.com>
2024-01-24 18:35:01 -0500

    Time: 314 ms (56.07%), Space: 30.2 MB (91.98%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,21 +1,15 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        # we do not care about negative numbers and 0
-        # if correctly ordered, nums[i] == i + 1
-        # while nums[i] != i, swap nums[i] with nums[nums[i]]
-        # place all numbers on the correct index. Iterate through the nums. If there isi a mis match, return the missing positive
-        # otherwise, return len(nums) + 1
         i = 0
         while i < len(nums):
-            targetIndex = nums[i] - 1
-            if 0 <= targetIndex < len(nums) and nums[i] != nums[targetIndex]:
-                nums[i], nums[targetIndex] = nums[targetIndex], nums[i]
+            # any positive nums[i] should be placed on nums[i] - 1: [1, 2, 3 ... ]
+            target = nums[i] - 1
+            if 0 <= target < len(nums) and nums[target] != nums[i]:
+                nums[target], nums[i] = nums[i], nums[target]
             else:
+                # move on
                 i += 1
-        
-        for i in range(len(nums)):
-            if nums[i] != i + 1:
+        for i, n in enumerate(nums):
+            if i + 1 != n:
                 return i + 1
         return len(nums) + 1
-                
-        
