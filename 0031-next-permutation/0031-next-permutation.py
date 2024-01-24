a32e6eec8c5178ae6736580c99260cb8b32bff45
Haojun Yuan <haojundev@gmail.com>
2024-01-23 19:46:50 -0500

    Time: 43 ms (74.53%), Space: 16.5 MB (59.13%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,16 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # get all permutations and compare
         for i in range(len(nums) - 1, -1, -1):
             if i > 0 and nums[i] > nums[i - 1]:
-                # swap the two indices
-                # sort nums[i + 1:]
-                for j in range(len(nums) -1, i - 1, -1):
+                for j in range(len(nums) - 1, i - 1, -1):
                     if nums[j] > nums[i - 1]:
-                        nums[i - 1], nums[j] = nums[j], nums[i - 1]
+                        nums[j], nums[i - 1] = nums[i - 1], nums[j]
                         nums[i:] = sorted(nums[i:])
                         return
         else:
-            nums.reverse()  # Reverse the list in-place
+            nums.reverse()
             return
+                        
