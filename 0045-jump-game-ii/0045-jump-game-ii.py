90718b9a336406ad090c5db4226c7eebd1105b70
Haojun Yuan <haojundev@gmail.com>
2024-01-26 11:31:18 -0500

    Time: 4394 ms (15.52%), Space: 17.4 MB (91.09%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,20 +1,22 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return 0  # If there's only one element, no jumps are needed.
-
-        max_reach = nums[0]  # Maximum reachable position in the current jump.
-        steps = 1  # Number of jumps taken so far.
-        current_max_reach = nums[0]  # Maximum reachable position in the next jump.
-
-        for i in range(1, n):
-            if i > max_reach:
-                # We need to take a jump because we cannot reach the current position.
-                steps += 1
-                max_reach = current_max_reach
-
-            # Update the maximum reachable position for the next jump.
-            current_max_reach = max(current_max_reach, i + nums[i])
-
-        return steps
+        dp = [math.inf] * len(nums)
+        dp[len(nums) - 1] = 0
+        for i in range(len(nums) - 2, -1, -1):
+            for j in range(1, nums[i] + 1):
+                if i + j < len(nums):
+                    dp[i] = min(dp[i], 1 + dp[i + j])
+        return dp[0]
+        
+#         res = self.process(0, nums)
+#         return res
+    
+#     def process(self, start, nums):
+#         if start == len(nums) - 1:
+#             return 0
+#         if start + nums[start] >= len(nums) - 1:
+#             return 1
+#         res = math.inf
+#         for i in range(1, nums[start] + 1):
+#             res = min(res, self.process(start + i, nums))
+#         return 1 + res
