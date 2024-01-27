0dad159e9c80a61fe5056f511e52aeb37cfda4d8
Haojun Yuan <haojundev@gmail.com>
2024-01-27 14:25:42 -0500

    Time: 99 ms (22.63%), Space: 16.9 MB (66.08%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,23 +1,27 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        # create board
         self.res = []
         board = [['.'] * n for _ in range(n)]
         self.solve(0, board, n)
         return self.res
     
-    def solve(self, r, board, n):
-        if r == n:
+    def solve(self, row, board, n):
+        # for every col of this row, try to put a queen there
+        # base case
+        if row == n:
             self.res.append([''.join(row) for row in board])
             return
-        # else we try to place a queen on each column and backrack
-        for c in range(n):
-            if self.isValid(board, r, c):
-                board[r][c] = 'Q'
-                self.solve(r + 1, board, n)
-                board[r][c] = '.'
+        for col in range(n):
+            if self.isValid(board, row, col):
+                board[row][col] = 'Q'
+                self.solve(row + 1, board, n)
+                board[row][col] = '.'
+                
+    
     def isValid(self, board, row, col):
-        # check all previous rolls
+        # row, col and diagonals
         for r in range(row):
-            if board[r][col] == 'Q' or abs(row - r) == abs(col - board[r].index('Q')):
+            if board[r][col] == 'Q' or r - board[r].index('Q') == row - col or r + board[r].index('Q') == row + col:
                 return False
         return True
