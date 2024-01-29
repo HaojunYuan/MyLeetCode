fade31129d99045992d40d5fdd1db84f13710e2e
Haojun Yuan <haojundev@gmail.com>
2024-01-28 23:40:38 -0500

    Time: 94 ms (79.92%), Space: 20.2 MB (51.18%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,41 +1,43 @@
 class Solution:
-#     def minDistance(self, word1: str, word2: str) -> int:
-#         return self.process(word1, word2, 0, 0)
-    
-#     def process(self, word1, word2, i, j):
-#         # operations needed to match word1[i:] and word2[j:]
-#         if i == len(word1) and j == len(word2):
-#             return 0
-#         if i == len(word1):
-#             return len(word2) - j
-#         if j == len(word2):
-#             return len(word1) - i
-#         if word1[i] == word2[j]:
-#             return self.process(word1, word2, i + 1, j + 1)
-#         p1 = 1 + self.process(word1, word2, i + 1, j)
-#         p2 = 1 + self.process(word1, word2, i, j + 1)
-#         p3 = 1 + self.process(word1, word2, i + 1, j + 1)
-#         return min(p1, p2, p3)
     def minDistance(self, word1: str, word2: str) -> int:
-        len1, len2 = len(word1), len(word2)
+        l1, l2 = len(word1), len(word2)
         if not word1:
-            return len2
-        elif not word2:
-            return len1
-        dp = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
-        # dp[len1][len2] = 0
-        for j in range(len2):
-            dp[-1][j] = len2 - j
-        for i in range(len1):
-            dp[i][-1] = len1 - i
-        for i in range(len1 - 1, -1, -1):
-            for j in range(len2 - 1, -1, -1):
+            return l2
+        if not word2:
+            return l1
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+        for j in range(l2):
+            dp[-1][j] = l2 - j
+        for i in range(l1):
+            dp[i][-1] = l1 - i
+        for i in range(l1 - 1, -1, -1):
+            for j in range(l2 - 1, -1, -1):
                 if word1[i] == word2[j]:
                     dp[i][j] = dp[i + 1][j + 1]
                 else:
-                    p1 = 1 + dp[i + 1][j]
-                    p2 = 1 + dp[i][j + 1]
-                    p3 = 1 + dp[i + 1][j + 1]
-                    dp[i][j] = min(p1, p2, p3)
+                    p1 = dp[i][j + 1]
+                    p2 = dp[i + 1][j]
+                    p3 = dp[i + 1][j + 1]
+                    dp[i][j] = 1 + min(p1, p2, p3)
         return dp[0][0]
-        
+#         return self.process(word1, word2, 0, 0)
+    
+#     def process(self, str1, str2, s1, s2):
+#         # if we hit both end, return 0: nothing we can do
+#         if s1 == len(str1) and s2 == len(str2):
+#             return 0
+#         # if we hit the end of one str, we can simply delete all extra chars
+#         if s1 == len(str1):
+#             return len(str2) - s2
+#         if s2 == len(str2):
+#             return len(str1) - s1
+#         # in case two chars are the same
+#         if str1[s1] == str2[s2]:
+#             return self.process(str1, str2, s1 + 1, s2 + 1)
+#         # 1. Move s1 to right, try to find a match
+#         # 2. Move s2 to right
+#         # 3. Take a step to replace a character, then move both pointers to right
+#         p1 = 1 + self.process(str1, str2, s1 + 1, s2)
+#         p2 = 1 + self.process(str1, str2, s1, s2 + 1)
+#         p3 = 1 + self.process(str1, str2, s1 + 1, s2 + 1)
+#         return min(p1, p2, p3)
