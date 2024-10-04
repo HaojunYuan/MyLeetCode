34e9ecebc910f4ac17e1383bb837c8020a3e201a
Haojun Yuan <haojundev@gmail.com>
2024-10-03 21:12:53 -0700

    Time: 36 ms (32.68%), Space: 12.3 MB (49.37%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,5 +1,10 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = l + (r - l) // 2
