2c2309f022c441d752ba2510cd31f88b8b74f72a
Haojun Yuan <haojundev@gmail.com>
2024-10-20 13:41:59 -0700

    Time: 10 ms (98.91%), Space: 12.7 MB (98.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,17 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        # one pass
-        major = None
-        count = 0
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        c = 0
+        major = nums[0]
         for n in nums:
-            if count == 0:
-                major = n
-            count += 1 if n == major else -1
+            if n == major:
+                c += 1
+            else:
+                c -= 1
+                if c < 0:
+                    major = n
+                    c = 1
         return major
-            
