3e4699116370828d1ef405a23ae572ae693b8244
Haojun Yuan <haojundev@gmail.com>
2024-10-27 15:39:47 -0700

    Time: 0 ms (100.00%), Space: 16.4 MB (99.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
-            return
+            return []
         self.dic = {
             "2": "abc",
             "3": "def",
@@ -17,15 +13,14 @@ class Solution(object):
             "9": "wxyz",
         }
         self.res = []
-        self.process(digits, 0, [])
+        self.process(0, digits, [])
         return self.res
     
-    def process(self, digits, start, path):
+    def process(self, start, digits, path):
         if start == len(digits):
             self.res.append(''.join(path))
             return
-        letters = self.dic[digits[start]]
-        for c in letters:
-            path.append(c)
-            self.process(digits, start + 1, path)
+        for char in self.dic[digits[start]]:
+            path.append(char)
+            self.process(start + 1, digits, path)
             path.pop()
