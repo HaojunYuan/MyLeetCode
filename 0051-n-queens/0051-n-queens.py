028bd35680545b04a01bcbfa52d0141643e15b23
Haojun Yuan <haojundev@gmail.com>
2024-10-27 16:38:06 -0700

    Time: 62 ms (12.75%), Space: 17.2 MB (25.33%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,27 +1,26 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # create board
+        # board
         self.res = []
         board = [['.'] * n for _ in range(n)]
         self.solve(0, board, n)
         return self.res
-    
+        
     def solve(self, row, board, n):
-        # for every col of this row, try to put a queen there
         # base case
         if row == n:
             self.res.append([''.join(row) for row in board])
             return
         for col in range(n):
-            if self.isValid(board, row, col):
+            if self.isValid(row, col, board):
                 board[row][col] = 'Q'
                 self.solve(row + 1, board, n)
                 board[row][col] = '.'
                 
-    
-    def isValid(self, board, row, col):
-        # row, col and diagonals
+    def isValid(self, row, col, board):
+        # check queens in previous rows
         for r in range(row):
             if board[r][col] == 'Q' or r - board[r].index('Q') == row - col or r + board[r].index('Q') == row + col:
                 return False
         return True
+            
