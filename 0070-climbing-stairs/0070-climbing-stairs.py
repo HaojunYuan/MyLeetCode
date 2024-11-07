0f3da95e786a12d1df047634ce30ca50065a78a9
Haojun Yuan <haojundev@gmail.com>
2024-11-06 19:45:34 -0800

    Time: 0 ms (100.00%), Space: 16.7 MB (15.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,19 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # # dp array
-        # dp = [0] * (n + 1)
-        # dp[0] = 1
-        # dp[1] = 1
-        # for i in range(2, n + 1):
-        #     dp[i] = dp[i - 1] + dp[i - 2]
-        # return dp[n]
-        
-        # Fibonacci Number
-        first = 1
-        second = 1
-        for i in range(2, n + 1):
-            third = first + second
-            first = second
-            second = third
-        return second
+        if n < 3:
+            return n
+        dp = [0] * n
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
         
