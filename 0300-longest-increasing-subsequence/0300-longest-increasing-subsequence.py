04efad19d8654cd2ed9c0044ade056c6f4c64cd8
Haojun Yuan <haojundev@gmail.com>
2024-11-12 23:14:56 -0800

    Time: 1808 ms (39.68%), Space: 17 MB (20.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -4,11 +4,11 @@ class Solution:
         # iterate through nums with index
         # for each index, iterate through all prior elements. Increment the index by dp[j] if nums[j] is smaller than nums[i]
         # update result, since the maximum subsequence might not be at the end, so we have to update the result every time we checked a subsequence
-        dp=[1]*len(nums)
-        res=1
-        for i in range(len(nums)):
+        n = len(nums)
+        dp = [1] * n
+        
+        for i in range(n):
             for j in range(i):
-                if nums[j]<nums[i]:
-                    dp[i]=max(dp[i],dp[j]+1)
-                    res=max(res,dp[i])
-        return res
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
